289a80aa7527af9a23573c3c5ab66ae713712f9f
Louis Brunner <louis.brunner.fr@gmail.com>
2019-10-12 13:37:05 +0100

    Fix ghpages build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  webpack (config, options) {
+  assetPrefix: isProd ? '/mes-pronoms/' : '/',
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    }
+  },
+  webpack(config, options) {
     config.resolve.modules = [__dirname, 'node_modules']
     return config
-  }
+  },
 }
